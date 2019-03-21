0e01f5d25e77d4a1532f5e17d2627b1272ea0d74
Abhijit Taware <ataware@DataTemperature.casa.cs.unb.ca>
2019-03-21 14:20:00 -0300

    maze aligned

--- flyWeight.facade/CS4015DP6Java_Abhijit/src/flyWeightTest.java
+++ flyWeight.facade/CS4015DP6Java_Abhijit/src/flyWeightTest.java
@@ -1,13 +1,38 @@
 //
 // This acts as a client for the assignment.
-
+import java.util.Scanner;
 //
 public class flyWeightTest {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        Maze maze = new Maze(6, 6);
-        System.out.println(maze.generate());
+
+		System.out.println("M <n>");
+		System.out.println("G <n>");
+		System.out.println("Q");
+		System.out.println("");
+		
+		int dim = 6;
+		while(true) {
+			Scanner scan = new Scanner(System.in);
+			char cmd = scan.next().charAt(0);
+			switch(cmd) {
+				case 'm':
+				case 'M':
+					dim = scan.nextInt();
+					Maze maze = new Maze(dim, dim);
+					System.out.println(maze.generate());
+					break;
+				case 'g':
+				case 'G':
+					dim = scan.nextInt();
+					System.out.println("TBD");
+					break;
+				case 'q':
+				case 'Q' :
+					return;
+			}
+
+		}
 	}
 
 }
